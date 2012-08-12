1f29263c5b2d31486132e44f9d3e1d765185ae32
Marcin <Marcin@Marcin-Komputer>
2012-08-12 11:09:55 +0200

    first comit

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: Marcin
+ *
+ * Created on 12 sierpień 2012, 11:07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Hello world!\n");
+
+    return (EXIT_SUCCESS);
+}
+
